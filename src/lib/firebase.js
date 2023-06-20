f55584201266ee3ec36181d4f07b0e00e97b74d5
home <home@MacBook-Pro-home.local>
2023-06-20 11:17:14 +0300

    Fix

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyA5sXaVrehT0bumEfD7ehuZ8D7q-3oslxk",
-  authDomain: "ecommerce-react-f5107.firebaseapp.com",
-  projectId: "ecommerce-react-f5107",
-  storageBucket: "ecommerce-react-f5107.appspot.com",
-  messagingSenderId: "13621463763",
-  appId: "1:13621463763:web:1c4cb94d814a920e652226",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
+
 
 export default firebase;
